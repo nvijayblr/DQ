67dfed9bf24c8995190979c3f0404f04e84ca282
Krishna <krishna.ez21@gmail.com>
2021-07-15 17:28:27 +0530

    Dashboard Page

--- src/app/components/authorized/main-dashboard/main-dashboard.component.ts
+++ src/app/components/authorized/main-dashboard/main-dashboard.component.ts
@@ -5,20 +5,33 @@ import { MatTableDataSource } from '@angular/material/table';
 import { HttpService } from 'src/app/services/http-service.service';
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
-  styleUrls: ['./main-dashboard.component.scss']
+  styleUrls: ['./main-dashboard.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+
 })
 export class MainDashboardComponent implements OnInit {
   displayedColumns: string[] = ['completeness', 'Accuracy', 'Uniqueness', 'Validity'];
   dataSource;
+  displayedColumnsDetails: string[] = ['uploadDate', 'sourceName', 'completeness', 'Accuracy', 'Uniqueness', 'Validity'];
+  dataSourceDetails;
+  expandedElement: PeriodicElementDetails | null;
 
   @ViewChild(MatPaginator, { static:false}) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild('table1', { read: MatSort, static: true }) sort: MatSort;
+  @ViewChild('table2', { read: MatSort, static: true }) sortDetails: MatSort;
   userLogin;
   getDB: FormGroup;
   constructor(private http: HttpService, private fb: FormBuilder,) {
@@ -38,48 +51,77 @@ export class MainDashboardComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+
+    this.dataSourceDetails.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceDetails.paginator) {
+      this.dataSourceDetails.paginator.firstPage();
+    }
   }
   allSourceCategory;
   sourceByCategory:any = [];;
   newObj;
   taskDetails: any = [];
   uploadDate: any = [];
+  detailedResults: any;
+  showDetailedTable: boolean = false;
   getSourceResults() {
     const payload = {
       username : this.userLogin.name,
     }
     this.http.getSourceResults(payload).subscribe((result: any) => {
-      this.allSourceCategory = result;
-      console.log(result);
-      this.allSourceCategory.map((result, i) => {
-        this.taskDetails.push({'sourceID' : result.sourceId, 'sourceDes' : result.sourceDesc, 'sourceName' : result.sourceName })
+      this.allSourceCategory = result.Aggresults;
+      this.detailedResults = result.detailedResults;
+      console.log(this.detailedResults);
+      this.detailedResults.map((result, i) => {
+       
+        result.detailedResults.map((res, i) => {
+          this.taskDetails.push({
+            'sourceName': result.sourceName,
+            'uploadDate': res.uploadDate,
+            'completeness': res.results.completeness,
+            'Accuracy': res.results.Accuracy,
+            'Uniqueness': res.results.Uniqueness,
+            'Validity': res.results.Validity,
+            "description": result.AggSubResults
+          })
+        })
+        
       })
-      Object.keys(this.allSourceCategory).map((key, index) => {
-        this.newObj = this.allSourceCategory[key].results.map((val, i) => {
-          return val;
-        });
-      });
-      this.newObj.map((key, index) => {
-        this.uploadDate.push(key.uploadDate);
-        this.sourceByCategory.push(key.results);
-      })
+      console.log(this.taskDetails)
 
-      console.log(this.uploadDate);
-
-      //console.log(moment(this.uploadDate).format('MM-DD-YYYY'));
-           
-      const ELEMENT_DATA: PeriodicElement[] = this.sourceByCategory;
+      const ELEMENT_DATA: PeriodicElement[] = [this.allSourceCategory];
       this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
       this.dataSource.sort = this.sort;
        this.dataSource.paginator = this.paginator;
     });
   }
+
+  getDetailedResults() {
+    this.showDetailedTable = true;
+    const ELEMENT_DATA: PeriodicElementDetails[] = this.taskDetails;
+      this.dataSourceDetails = new MatTableDataSource<PeriodicElementDetails>(ELEMENT_DATA);
+      this.dataSourceDetails.sort = this.sortDetails;
+       this.dataSourceDetails.paginator = this.paginator;   
+  }
 }
 
 
+
+
 export interface PeriodicElement {
-  completeness: string;
+  completeness: number;
   Accuracy: number;
   Uniqueness: number;
-  Validity: string;
+  Validity: number;
+}
+
+export interface PeriodicElementDetails {
+  sourceName: string;
+  uploadDate: string;
+  completeness: number;
+  Accuracy: number;
+  Uniqueness: number;
+  Validity: number;
+  description: string;
 }
