104b34df906ecbba134574e040161c115f2c9962
Vijay <nvijay.blr@gmail.com>
2021-07-26 10:42:45 +0530

    Advanced formula editor

--- src/app/components/authorized/data-quality/data-quality.component.ts
+++ src/app/components/authorized/data-quality/data-quality.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, ViewChild, OnInit, TemplateRef} from '@angular
 import { HttpService } from 'src/app/services/http-service.service';
 import * as _ from 'lodash';
 import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl, FormControl } from '@angular/forms';
-import { Angular2Csv } from 'angular2-csv';
+// import { Angular2Csv } from 'angular2-csv';
 
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
@@ -15,11 +15,14 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 })
 
 
-  
-  
+
+
 export class DataQualityComponent implements OnInit {
-  @ViewChild("contentMsg", { static: false }) modalContent: TemplateRef<any>;
-  @ViewChild("contentErr", {static: false}) modalErrContent: TemplateRef<any>;
+  constructor(private http: HttpService, private fb: FormBuilder, private modalService: NgbModal) {
+
+  }
+  @ViewChild('contentMsg', { static: false }) modalContent: TemplateRef<any>;
+  @ViewChild('contentErr', {static: false}) modalErrContent: TemplateRef<any>;
   isPreviewLoaded = false;
   isPreviewLoading = false;
   defaultColDefs = { sortable: true, filter: true, minWidth: 180, resizable: true };
@@ -29,11 +32,59 @@ export class DataQualityComponent implements OnInit {
   closeResult = '';
   globalData;
   globalDataPath;
-  loadingGlobalData: boolean = false;
-  loadingLocalData:boolean = false;
-  constructor(private http: HttpService, private fb: FormBuilder,private modalService: NgbModal) {
-  
-  }
+  loadingGlobalData = false;
+  loadingLocalData = false;
+
+
+
+
+
+  searchValue;
+  searchResult: any = [];
+  isLoading = false;
+  allObjHeader: any = [];
+  allObjKeys: any = [];
+  allObjKeysExt;
+  allObjValues: any = [];
+  collections: any = [];
+
+  db;
+  dbCollections;
+  showAllDetails = false;
+  isLoadingDB = false;
+  newDB;
+  dbValues: any = [];
+  clientUrl;
+  uploadButton = false;
+  alertErrMessage;
+  isLoadingCDB = false;
+
+
+  isLoadingCO = false;
+  isLoadingBt = false;
+  collectionTable: any = [];
+  cityKey;
+  collectionKey: any = [];
+  collectionValue: any = [];
+  selectdItem;
+  selectedColumn;
+  isButtonShow = false;
+  startIndex = 0;
+  endIndex = 100;
+
+  collectionResult;
+  titleSrc;
+
+
+  dataSource;
+  urlValue;
+  fileName;
+  alertMessage;
+  savePath;
+
+
+  status: any[];
+  formula = 'mongoDb';
 
   ngOnInit() {
     this.getDB = this.fb.group({
@@ -43,33 +94,20 @@ export class DataQualityComponent implements OnInit {
     this.globalData = localStorage.getItem('globalData');
     if (!this.globalData) {
       this.loadingLocalData = true;
-      this.getDBCollections();  
+      this.getDBCollections();
     } else {
       this.loadingGlobalData = true;
       this.globalDataPath = JSON.parse(this.globalData);
     }
     console.log(this.globalDataPath.Ref_data_files);
-    
+
   }
 
-  
- 
-
-  
-  searchValue;
-  searchResult: any = [];
-  isLoading: boolean = false;
-  allObjHeader: any = [];
-  allObjKeys: any = [];
-  allObjKeysExt;
-  allObjValues: any = [];
-  collections: any = [];
-    
   applySearch(event: Event) {
     this.searchValue = event;
     const payload = {
       query: this.searchValue
-    }
+    };
     this.isLoading = true;
     this.http.getSearchCollections(payload).subscribe((result: any) => {
       this.isLoading = false;
@@ -77,97 +115,73 @@ export class DataQualityComponent implements OnInit {
       this.allObjHeader = _.keys(this.searchResult);
       this.allObjValues = _.values(this.searchResult);
       for (let i = 0; i < this.allObjValues.length; i++) {
-        this.allObjKeys.push(_.keys(this.allObjValues[i][0]))
-      };
+        this.allObjKeys.push(_.keys(this.allObjValues[i][0]));
+      }
       for (let i = 0; i < this.allObjHeader.length; i++) {
-        this.allObjKeysExt = this.searchResult[this.allObjHeader[i]].map(function (key, value) {
+        this.allObjKeysExt = this.searchResult[this.allObjHeader[i]].map(function(key, value) {
           return _.values(key);
         });
         this.collections.push(this.allObjKeysExt);
-      };
+      }
 
     }, (error) => {
       alert(error.message);
     });
   }
-
-  db;
-  dbCollections;
-  showAllDetails: boolean = false;
-  isLoadingDB: boolean = false;
-  newDB;
-  dbValues: any = [];
-  clientUrl;
-  uploadButton : boolean = false;
   getDBCollections() {
     this.isLoadingDB = true;
     this.showAllDetails = false;
     const payload = {
-      client_url: this.clientUrl || "",
-    }
+      client_url: this.clientUrl || '',
+    };
     this.http.getDBCollections(payload).subscribe((result: any) => {
       this.newDB = result.Cluster_Contents;
       this.db = _.keys(result.Cluster_Contents);
       this.dbValues.push(this.newDB);
       this.dataSource = _.values(result.Cluster_Contents);
       this.isLoadingDB = false;
-      this.showAllDetails = true
+      this.showAllDetails = true;
     }, (error) => {
       alert(error);
-    })
+    });
   }
-  alertErrMessage;
-  isLoadingCDB = false;
   getDBCollectionsClient() {
     this.isLoadingCDB = true;
     this.showAllDetails = false;
     this.uploadButton = false;
     const payload = {
-      client_url: this.clientUrl || "",
-    }
+      client_url: this.clientUrl || '',
+    };
     this.http.getDBCollections(payload).subscribe((result: any) => {
-      console.log(result)
+      console.log(result);
       this.uploadButton = true;
       this.newDB = result.Cluster_Contents;
       this.db = _.keys(result.Cluster_Contents);
       this.dbValues.push(this.newDB);
       this.dataSource = _.values(result.Cluster_Contents);
       this.isLoadingCDB = false;
-      this.showAllDetails = true
+      this.showAllDetails = true;
     }, (error) => {
       this.alertErrMessage = error.message;
       this.isLoadingCDB = false;
-      this.modalService.open(this.modalErrContent, { windowClass: 'modal-holder' });           
-    })
+      this.modalService.open(this.modalErrContent, { windowClass: 'modal-holder' });
+    });
   }
 
   onOptionsSelected(value: string) {
-    this.dbCollections = this.newDB[value]
+    this.dbCollections = this.newDB[value];
   }
-  
-
-  isLoadingCO: boolean = false;
-  isLoadingBt: boolean = false;
-  collectionTable: any = [];
-  cityKey;
-  collectionKey: any = [];
-  collectionValue: any = [];
-  selectdItem;
-  selectedColumn;
-  isButtonShow: boolean = false;
-  startIndex = 0;
-  endIndex = 100;
 
   loadMoreDb() {
     this.startIndex = this.endIndex;
     this.endIndex = this.endIndex + 100;
     const payload = {
-      client_url: "",
+      client_url: '',
       db: this.selectdItem,
       collection: this.selectedColumn,
       start_index: this.startIndex,
       end_index: this.endIndex
-    }
+    };
     this.isLoadingBt = true;
     this.http.getDBPreview(payload).subscribe((result: any) => {
       this.isLoadingBt = false;
@@ -186,21 +200,19 @@ export class DataQualityComponent implements OnInit {
         });
       }
       this.rowData = JSON.parse(JSON.stringify(this.rowData));
-    })
+    });
   }
-
-  collectionResult;
-  getDBPreview(item, column) {    
+  getDBPreview(item, column) {
     this.selectedColumn = column.toString();
     this.selectdItem = item;
     this.isButtonShow = false;
     const payload = {
-      client_url : "",
+      client_url : '',
       db: this.selectdItem,
       collection: this.selectedColumn,
       start_index: this.startIndex,
       end_index : this.endIndex
-    }
+    };
     this.isLoadingCO = true;
     this.http.getDBPreview(payload).subscribe((result: any) => {
       this.collectionResult = result.Preview;
@@ -209,35 +221,34 @@ export class DataQualityComponent implements OnInit {
       this.rowData = [];
       this.columnDefs = [];
       this.collectionTable = result.Preview;
-      if (this.collectionTable) {     
+      if (this.collectionTable) {
         this.previewTable();
     }
-  
+
       this.isPreviewLoaded = true;
       this.isPreviewLoading = false;
 
     }, (error) => {
       this.isLoadingCO = false;
       alert(error.message);
-    })
+    });
   }
-  titleSrc;
   loadReferencePreview(path) {
-    this.titleSrc = path;   
+    this.titleSrc = path;
     const payload = {
       sourcepath: this.titleSrc
     };
     this.isLoadingCO = true;
-    this.http.getProfileView(payload).subscribe((res: any) => {      
+    this.http.getProfileView(payload).subscribe((res: any) => {
       this.collectionResult = res.Preview;
       this.isLoadingCO = false;
       this.rowData = [];
       this.columnDefs = [];
       this.collectionTable = res.Preview;
-      if (this.collectionTable) {     
+      if (this.collectionTable) {
         this.previewTable();
     }
-  
+
       this.isPreviewLoaded = true;
       this.isPreviewLoading = false;
       }, (error) => {
@@ -262,25 +273,18 @@ export class DataQualityComponent implements OnInit {
       });
     }
   }
-  
-
-  dataSource;
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  urlValue;
-  fileName;
-  alertMessage;
-  savePath;
-  open(saveFile, item, column) {    
+  open(saveFile, item, column) {
     this.modalService.open(saveFile, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      const payload = {  
+      const payload = {
           client_url: result.urlValue || '',
           db: item,
-          collection:column,   
+          collection: column,
           output_filename : result.fileName + '.csv',
-        }
+        };
       this.http.saveMangoDbCollection(payload).subscribe((result: any) => {
           console.log(result);
           this.alertMessage = result.Message;
@@ -290,7 +294,7 @@ export class DataQualityComponent implements OnInit {
           this.isLoading = false;
         });
       this.closeResult = `Closed with: ${result}`;
-      
+
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
@@ -300,7 +304,7 @@ export class DataQualityComponent implements OnInit {
     this.modalService.open(content, { windowClass: 'modal-holder' }).result.then((result) => {
       this.clientUrl = result;
       this.getDBCollectionsClient();
-    })
+    });
   }
 
   private getDismissReason(reason: any): string {
@@ -313,31 +317,27 @@ export class DataQualityComponent implements OnInit {
     }
   }
 
-
-  status: any[];
-  formula: string = 'mongoDb';
-  
   downloadCSV() {
-    this.status = ['approved', 'rejected', 'pending'];
-    let data = this.collectionResult;
-    console.log(data);
+    // this.status = ['approved', 'rejected', 'pending'];
+    // const data = this.collectionResult;
+    // console.log(data);
 
-    let fileHeaders = [];
-    Object.keys(this.collectionResult[0]).map((key, index) => {     
-      fileHeaders.push(key);
-    });
-    let options = {
-      title: '',
-      fieldSeparator: ',',
-      quoteStrings: '"',
-      decimalseparator: '.',
-      //showLabels: true,
-      //showTitle: true,
-      //useBom: true,
-      headers: fileHeaders
-    };
+    // const fileHeaders = [];
+    // Object.keys(this.collectionResult[0]).map((key, index) => {
+    //   fileHeaders.push(key);
+    // });
+    // const options = {
+    //   title: '',
+    //   fieldSeparator: ',',
+    //   quoteStrings: '"',
+    //   decimalseparator: '.',
+    //   // showLabels: true,
+    //   // showTitle: true,
+    //   // useBom: true,
+    //   headers: fileHeaders
+    // };
 
-    new Angular2Csv(data, this.formula, options);
+    // new Angular2Csv(data, this.formula, options);
   }
 
 }
