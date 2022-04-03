581bcbea8189332a9747081bca8864b749338cb9
SugapriyaMani <sugapriya.mani@aidatadriven.com>
2022-04-03 18:25:16 +0530

    Filter for preview

--- src/app/components/data-driven/preview-detail/preview-detail.component.ts
+++ src/app/components/data-driven/preview-detail/preview-detail.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from '../../../services/http-service.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { AlertService } from 'src/app/shared/alert-dialog/alert-dialog.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-preview-detail1',
@@ -14,16 +15,21 @@ export class PreviewDetailComponent {
     @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
     displayedColumns: string[] = [];
+    displayedColumns_Filter: string[] = [];
     previewDataSource: MatTableDataSource<any>;
+    filterForm: FormGroup;
     isLoading: boolean = false;
 
     constructor(private http: HttpService,
-        private alertService: AlertService) { }
+        private alertService: AlertService) {
+        this.filterForm = new FormGroup({});
+    }
 
     loadSourcePreview() {
         this.isLoading = true;
         this.previewDataSource = new MatTableDataSource([]);
         this.displayedColumns = [];
+        this.displayedColumns_Filter = [];
         const payload = {
             sourcepath: this.source.templateSourcePath,
             seeMoreEnabled: 'NO',
@@ -42,6 +48,7 @@ export class PreviewDetailComponent {
     parseSourcePreview(details) {
         let rowData = [];
         this.displayedColumns = [];
+        this.displayedColumns_Filter = [];
         Object.keys(details).map((key, index) => {
             rowData.push({
                 ...details[key]
@@ -51,9 +58,47 @@ export class PreviewDetailComponent {
         if (rowData.length) {
             Object.keys(rowData[0]).map((key, index) => {
                 this.displayedColumns.push(key);
+                this.displayedColumns_Filter.push(key + '_filter');
             });
         }
         this.previewDataSource = new MatTableDataSource(rowData);
         this.previewDataSource.paginator = this.paginator;
+        this.previewDataSource.filterPredicate = this.createFilter();
+        this.initFilterForm();
     }
+
+    initFilterForm() {
+        let group = {}
+        this.displayedColumns.forEach(column => {
+            group[column] = new FormControl('');
+        })
+        this.filterForm = new FormGroup(group);
+
+        this.filterForm.valueChanges.subscribe(value => {
+            this.setFilter();
+        });
+    }
+
+    setFilter() {
+        let filter = {
+            ...this.filterForm.value
+        };
+        filter = JSON.stringify(filter);
+        this.previewDataSource.filter = filter;
+    }
+
+    createFilter() {
+        let filterFunction = function (data: any, filter: any): boolean {
+            let result = true;
+            filter = JSON.parse(filter);
+            Object.keys(filter).forEach((column) => {
+                if (filter[column] && data[column] != undefined && data[column] != null) {
+                    result = result && data[column].toString().toLowerCase().indexOf(filter[column].toLowerCase()) >= 0;
+                }
+            });
+            return result;
+        }
+        return filterFunction;
+    }
+
 }
